6839fa41cf3a96c47e10b70c01b8e7ef38248ccc
m_thado <tmiseda@gmail.com>
2024-12-13 18:59:13 -0500

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h1>Welcome to Task Managers App</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+     <h1 className="text-3xl font-bold mb-4">Task Manager</h1>
+     <p>Welcome to your task management dashboard!</p>
     </div>
   );
 }
